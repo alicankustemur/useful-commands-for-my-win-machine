6c8889a259840aa9dbd7d789601a624c6d7cb419
alicankustemur <alican.kustemur@gmail.com>
2024-07-21 15:19:32 +0300

    Add useful commands for my windows 10 machine

---
+++ commands.sh
@@ -0,0 +1,11 @@
+# Find all .png files and rename them
+find . -type f -name '*.png' -printf "echo mv '%h/%f' '%h/%s_%f\n'" | sh
+
+# Find all .png files and move them to the upper folder
+find . -type f -name '*.png' -printf "echo mv '%h/%f' './\n'" | sh
+
+# Remove _8047 string from the png file recursively
+Get-ChildItem  -Filter "*png" | Rename-Item -NewName {$_.Name -replace '_8047'}
+
+# List all file types on the directory
+find . -type f | perl -ne 'print $1 if m/\.([^.\/]+)$/' | sort -u
